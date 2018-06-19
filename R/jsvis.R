aff5d52d1397557bbb7f64e4d1739e10bd5f9762
kcf-jackson <kcf.jackson@gmail.com>
2018-06-20 02:50:52 +1000

    Update version number, exports and documentation

--- R/jsvis.R
+++ R/jsvis.R
@@ -1,7 +1,26 @@
 #' jsvis
 #' @name jsvis
 #' @importFrom utils browseURL
-#' @importFrom magrittr %<>% %>%
-#' @export %<>% %>%
 #' @description This package facilitates the use of JavaScript in R for animated / interactive data visualisation.
 utils::globalVariables(".")
+
+#' Forward pipe operator
+#'
+#' @name %>%
+#' @rdname forward_pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+
+#' Compound assignment Pipe operator
+#'
+#' @name %<>%
+#' @rdname compound_pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %<>%
+#' @usage lhs \%<>\% rhs
+NULL
